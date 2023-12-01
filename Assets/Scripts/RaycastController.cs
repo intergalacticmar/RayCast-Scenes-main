98b92e433dd9e7c970a4af1237dd9782365f89da
María <120553496+intergalacticmar@users.noreply.github.com>
2023-12-01 10:22:51 +0100

    guardado 3

--- Assets/Scripts/RaycastController.cs
+++ Assets/Scripts/RaycastController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RaycastController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float raycastDistance = 10f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, raycastDistance))
+        {
+            if (hit.collider != null)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    OpenScene1();
+                }
+            }
+        }
+    }
+
+    void OpenScene1()
+    {
+        SceneManager.LoadScene("Scene1");
     }
 }
