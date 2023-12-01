c17d6575fc9fac0bbc22a3afb2a8f722b52839f4
María <120553496+intergalacticmar@users.noreply.github.com>
2023-12-01 09:43:59 +0100

    Guardado 1

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                Debug.Log("Objeto impactado: " + hit.collider.gameObject.name);
+            }
+        }
+    }
+}
+
